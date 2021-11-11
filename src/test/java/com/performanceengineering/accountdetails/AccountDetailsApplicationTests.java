351436dda7e7dca9f5ffb2e6280c5f18598ea620
Premraj Varadharaj Singaravelu <premrajvs@gmail.com>
2021-11-11 13:24:34 -0600

    init

---
+++ src/test/java/com/performanceengineering/accountdetails/AccountDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.performanceengineering.accountdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
